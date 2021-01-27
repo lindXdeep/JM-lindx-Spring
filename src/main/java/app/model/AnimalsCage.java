0a6967bb0ee3f2305263681b4d7fcc806d1b1de2
lindx <linder.igor@gmail.com>
2021-01-27 09:14:07 +0300

    doggy dog

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,14 +1,28 @@
 package app.model;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
+    public Timer getTimer(){
+        return timer;
+    }
+    
+    @PostConstruct
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
